c34f10b09174ea79b4a5ac1b3763a74f9d8d6ffe
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-04 19:20:01 +0200

    Add LocalDataSource & RemoteDataSource interfaces        +Add :          + Methods of Local Data Source.          + Methods of Remote Data Source.
    [design_data_module]

---
+++ data/src/main/java/com/proekspert/data/repository/LocalDataSource.kt
@@ -0,0 +1,18 @@
+package com.proekspert.data.repository
+
+import com.proekspert.data.model.MatchDTO
+import com.proekspert.data.model.MatchResultDTO
+
+/**
+ * Methods of Local Data Source
+ */
+interface LocalDataSource {
+
+    suspend fun addMatchBet(matchBet: MatchResultDTO): Long
+
+    suspend fun addAllMatches(matchesList: List<MatchDTO>) : List<Long>
+
+    suspend fun addAllMatchesResults(matchesResults: List<MatchResultDTO>): Long
+
+    suspend fun getMatchBet(team1: String, team2: String) : MatchResultDTO
+}
