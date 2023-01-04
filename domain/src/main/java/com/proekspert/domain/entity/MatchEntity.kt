8b181980c8f00ddde9af62e6a8c8eeaa17db6379
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-04 12:36:58 +0200

    Add domain entity's
    [design_domain_module]

---
+++ domain/src/main/java/com/proekspert/domain/entity/MatchEntity.kt
@@ -0,0 +1,6 @@
+package com.proekspert.domain.entity
+
+data class MatchEntity(
+    val team1:String,
+    val team2:String
+)
---
+++ domain/src/main/java/com/proekspert/domain/entity/ResultEntity.kt
@@ -0,0 +1,8 @@
+package com.proekspert.domain.entity
+
+data class ResultEntity(
+    val team1: String,
+    val team2: String,
+    val team1_points: Int,
+    val team2_points: Int
+)
