1f54755237c9fb03114cc6246a3043e48aaaef7e
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-04 19:37:11 +0200

    Update LocalDataSource interfaces        +Update :          + Methods of Local Data Source.
    [design_data_module]

--- data/src/main/java/com/proekspert/data/repository/LocalDataSource.kt
+++ data/src/main/java/com/proekspert/data/repository/LocalDataSource.kt
@@ -10,9 +10,19 @@ interface LocalDataSource {
 
     suspend fun addMatchBet(matchBet: MatchResultDTO): Long
 
-    suspend fun addAllMatches(matchesList: List<MatchDTO>) : List<Long>
+    suspend fun addAllMatches(matchesList: List<MatchDTO>): List<Long>
 
     suspend fun addAllMatchesResults(matchesResults: List<MatchResultDTO>): Long
 
-    suspend fun getMatchBet(team1: String, team2: String) : MatchResultDTO
+    suspend fun getMatchBet(team1: String, team2: String): MatchResultDTO
+
+    suspend fun getAllMatches(): List<MatchDTO>
+
+    suspend fun getAllMatchesResults(): List<MatchResultDTO>
+
+    suspend fun deleteAllMatchesBet()
+
+    suspend fun deleteAllMatches()
+
+    suspend fun deleteAllMatchesResults()
 }
