81e6dd0ef1983efdc30be833cdc3dd34bb2f67d6
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-05 20:37:08 +0200

    Add MatchesForPredictionContract
    [design_feature_module]

---
+++ feature/src/main/java/com/proekspert/feature/contract/MatchesForPredictionContract.kt
@@ -0,0 +1,34 @@
+package com.proekspert.feature.contract
+
+import com.proekspert.base.UiEffect
+import com.proekspert.base.UiEvent
+import com.proekspert.base.UiState
+import com.proekspert.feature.model.MatchUiModel
+
+
+/**
+ * Contract of Main Screen
+ */
+class MatchesForPredictionContract {
+
+    sealed class Event : UiEvent {
+        object OnFetchAllMatchesForPredictions : Event()
+        object GetAllMatchesResults : Event()
+        data class OnMatchItemClicked(val match: MatchUiModel) : Event()
+    }
+
+    data class State(
+        val matchState: MatchState,
+        val selectedMatch: MatchUiModel? = null
+    ) : UiState
+
+    sealed class MatchState {
+        object Idle : MatchState()
+        object Loading : MatchState()
+        data class Success(val authorList: List<MatchUiModel>) : MatchState()
+    }
+
+    sealed class Effect : UiEffect {
+        data class ShowError(val message: String?) : Effect()
+    }
+}
