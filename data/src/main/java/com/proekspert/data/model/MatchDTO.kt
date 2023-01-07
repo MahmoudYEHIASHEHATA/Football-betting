fd586d345ce5d9c59397be5d5657bc2212c67927
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-07 19:40:38 +0200

    Fix Data layer module
           +Update:
              + MatchDTO class name -> MatchModel.
              + MatchResultDTO class name -> MatchResultModel.
              + Update MatchDataDomainMapper calss to mape from/to <MatchModel, Match>
              + Update MatchResultDataDomainMapper calss to mape from/to <MatchResultModel, MatchResult>.
              + Update getAllMatches() in LocalDataSource TO return flow of data insted of List.
              + Update getAllMatchesResults() in LocalDataSource TO return flow of data insted of List.
    
           +Add :
             + Add two new variable to MatchModel to handle scor betting.
             + Add editMatch() to update betting.
    
    [design_app]

--- data/src/main/java/com/proekspert/data/model/MatchDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.proekspert.data.model
-
-data class MatchDTO(
-    val team1: String,
-    val team2: String
-)
--- data/src/main/java/com/proekspert/data/repository/RepositoryImp.kt
+++ data/src/main/java/com/proekspert/data/repository/RepositoryImp.kt
@@ -1,71 +1,64 @@
 package com.proekspert.data.repository
 
 import com.proekspert.common.Mapper
-import com.proekspert.common.Resource
-import com.proekspert.data.model.MatchDTO
-import com.proekspert.data.model.MatchResultDTO
-import com.proekspert.domain.entity.MatchEntity
-import com.proekspert.domain.entity.MatchResultEntity
+import com.proekspert.data.model.MatchModel
+import com.proekspert.data.model.MatchResultModel
+import com.proekspert.domain.model.Match
+import com.proekspert.domain.model.MatchResult
 import com.proekspert.domain.repository.Repository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class RepositoryImp @Inject constructor(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource,
-    private val matchMapper: Mapper<MatchDTO, MatchEntity>,
-    private val matchResultMapper: Mapper<MatchResultDTO, MatchResultEntity>
+    private val matchMapper: Mapper<MatchModel, Match>,
+    private val matchResultMapper: Mapper<MatchResultModel, MatchResult>
 ) : Repository {
-    override suspend fun getMatches(): Flow<Resource<List<MatchEntity>>> {
-        return flow {
-            try {
-                // Get data from RemoteDataSource
-                val data = remoteDataSource.getAllMatches()
-                //Delete the old matches if exist
-                localDataSource.deleteAllMatches()
-                // Save to local or update if exist
-                localDataSource.addAllMatches(data)
-                // Emit data
-                emit(Resource.Success(matchMapper.fromList(data)))
-            } catch (ex : Exception) {
-                // If remote request fails
-                try {
-                    // Get data from LocalDataSource
-                    val local = localDataSource.getAllMatches()
-                    // Emit data
-                    emit(Resource.Success(matchMapper.fromList(local)))
-                } catch (ex1 : Exception) {
-                    // Emit error
-                    emit(Resource.Error(ex1))
-                }
-            }
-        }
+    override suspend fun getMatches(): Flow<List<Match>> {
+        return localDataSource.getAllMatches()
+            .onStart {
+                refreshMatchesCache()
+            }.map { matchMapper.fromList(it) }
     }
 
-    override suspend fun getMatchesResults(): Flow<Resource<List<MatchResultEntity>>> {
-        return flow {
-            try {
-                // Get data from RemoteDataSource
-                val data = remoteDataSource.getAllMatchesResults()
-                //Delete the old matches results if exist
-                localDataSource.deleteAllMatchesResults()
-                // Save to local or update if exist
-                localDataSource.addAllMatchesResults(data)
-                // Emit data
-                emit(Resource.Success(matchResultMapper.fromList(data)))
-            } catch (ex : Exception) {
-                // If remote request fails
-                try {
-                    // Get data from LocalDataSource
-                    val local = localDataSource.getAllMatchesResults()
-                    // Emit data
-                    emit(Resource.Success(matchResultMapper.fromList(local)))
-                } catch (ex1 : Exception) {
-                    // Emit error
-                    emit(Resource.Error(ex1))
-                }
-            }
-        }
+    override suspend fun editMatch(match: Match) {
+        localDataSource.editMatch(matchMapper.to(match))
+    }
+
+    override suspend fun deleteAllMatches(): Int {
+        return localDataSource.deleteAllMatches()
+    }
+
+    override suspend fun deleteAllMatchesResults(): Int {
+        return localDataSource.deleteAllMatchesResults()
+    }
+
+    private suspend fun refreshMatchesCache() {
+        // Get data from RemoteDataSource
+        val data = remoteDataSource.getAllMatches()
+        //Delete the old matches if exist
+        localDataSource.deleteAllMatches()
+        // Save to local or update if exist
+        localDataSource.addAllMatches(data)
+    }
+
+    override suspend fun getMatchesResults(): Flow<List<MatchResult>> {
+        return localDataSource.getAllMatchesResults()
+            .onStart {
+                refreshMatchesResultsCache()
+            }.map { matchResultMapper.fromList(it) }
+    }
+
+
+    private suspend fun refreshMatchesResultsCache() {
+        // Get data from RemoteDataSource
+        val data = remoteDataSource.getAllMatchesResults()
+        //Delete the old matches results if exist
+        localDataSource.deleteAllMatchesResults()
+        // Save to local or update if exist
+        localDataSource.addAllMatchesResults(data)
     }
 }
