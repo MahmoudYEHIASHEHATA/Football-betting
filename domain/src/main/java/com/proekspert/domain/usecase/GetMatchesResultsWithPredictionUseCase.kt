4caa6a8a298f6218182fedce8b5410e132ec2556
Shehatah <mahmoudyehia.shehata@cassbana.com>
2023-01-08 16:30:29 +0200

    Update name of map prediction list
    [design_combine_betting_and_result_score]

--- domain/src/main/java/com/proekspert/domain/usecase/GetMatchesResultsWithPredictionUseCase.kt
+++ domain/src/main/java/com/proekspert/domain/usecase/GetMatchesResultsWithPredictionUseCase.kt
@@ -16,13 +16,13 @@ class GetMatchesResultsWithPredictionUseCase @Inject constructor(
     }
 
     private suspend fun getMatchesResultsWithPrediction(): Flow<List<MatchResultsWithPrediction>> {
-        var hashMap: HashMap<Pair<String, String>, Pair<Int?, Int?>> = HashMap()
+        var mapPredictions: HashMap<Pair<String, String>, Pair<Int?, Int?>> = HashMap()
         val matchesPredictions = repository.getCachedMatches()
         val matchesResults = repository.getMatchesResults()
 
         return matchesResults.combine(matchesPredictions) { results, predictions ->
             for (prediction in predictions) {
-                hashMap[Pair(prediction.team1, prediction.team2)] =
+                mapPredictions[Pair(prediction.team1, prediction.team2)] =
                     Pair(prediction.bettingScoreTeam1, prediction.bettingScoreTeam2)
             }
             results.map {
@@ -31,8 +31,8 @@ class GetMatchesResultsWithPredictionUseCase @Inject constructor(
                     it.team2,
                     it.team1_points,
                     it.team2_points,
-                    hashMap[Pair(it.team1, it.team2)]?.first,
-                    hashMap[Pair(it.team1, it.team2)]?.second
+                    mapPredictions[Pair(it.team1, it.team2)]?.first,
+                    mapPredictions[Pair(it.team1, it.team2)]?.second
                 )
             }
         }
